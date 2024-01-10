bc1e406dd733c608e2f4cf2858b8205f761547bf
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-10 17:50:08 +0300

    Link comp uppercase class eklendi

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -15,7 +15,7 @@ const Error = () => {
             Sorry, we couldn't find the page you're looking for
           </p>
           <div className='mt-10'>
-            <Link to='/' className='btn btn-secondary'>
+            <Link to='/' className='btn btn-secondary uppercase'>
               Go back home
             </Link>
           </div>
