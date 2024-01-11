fe591638aedb1f38db321ebf7c451f95a74e5d31
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-11 12:58:02 +0300

    padding revize

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -6,7 +6,7 @@ const HomeLayout = () => {
       <nav>
         <span className='text-4xl text-primary'>Comfy</span>
       </nav>
-      <section className='align-element py-8'>
+      <section className='align-element py-20'>
         <Outlet />
       </section>
     </>
