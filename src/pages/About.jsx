530ff23ad4921c75b2c47d3b850e69844cf9d341
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-16 19:17:16 +0300

    About component olusturuldu

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,28 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
+        <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+          We love
+        </h1>
+        <div className='stats bg-primary shadow'>
+          <div className='stat'>
+            <div className='stat-title text-primary-content text-4xl font-bold tracking-widest'>
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className='mt-6 text-lg leading-8 max-w-2xl mx-auto'>
+        Comfy store is awesome. If you come to comfy store, you will feel good.
+        Comfy store chooses the most suitable product for you. Lorem ipsum dolor
+        sit amet, consectetur adipisicing elit. Aperiam, asperiores doloribus.
+        Dolorem odio sapiente cumque veritatis fuga placeat nemo ipsum quidem
+        odit voluptatum eaque architecto consectetur enim facere cum tempore
+        nulla, fugiat quia esse.
+      </p>
+    </>
+  );
 };
 
 export default About;
