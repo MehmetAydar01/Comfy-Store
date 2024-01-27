e14845c13925f19e84c4bf23d808a06afb412ec1
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-28 02:36:52 +0300

    url'deki parametreler alindi ve config ile eklendi ve return edildi

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,10 +4,16 @@ import { customFetch } from '../utils';
 const url = '/products';
 
 export const Loader = async ({ request }) => {
-  const response = await customFetch(url);
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+
+  const response = await customFetch(url, {
+    params,
+  });
   const products = response.data.data;
   const meta = response.data.meta;
-  return { products, meta };
+  return { products, meta, params };
 };
 
 const Products = () => {
