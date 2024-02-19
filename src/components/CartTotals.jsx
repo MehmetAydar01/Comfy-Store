1368ed73775c77a89f5f9546e2302491248f4305
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-20 02:55:33 +0300

    stil duzenleme

--- src/components/CartTotals.jsx
+++ src/components/CartTotals.jsx
@@ -10,22 +10,22 @@ const CartTotals = () => {
     <div className='card bg-base-200'>
       <div className='card-body'>
         {/* SUBTOTAL */}
-        <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+        <p className='flex justify-between font-medium text-sm border-b border-base-300 pb-2'>
           <span>Subtotal</span>
-          <span className='font-medium'>{formatPrice(cartTotal)}</span>
+          <span className='font-semibold'>{formatPrice(cartTotal)}</span>
         </p>
         {/* SHIPPING */}
-        <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+        <p className='flex justify-between font-medium text-sm border-b border-base-300 pb-2'>
           <span>Shipping</span>
-          <span className='font-medium'>{formatPrice(shipping)}</span>
+          <span className='font-semibold'>{formatPrice(shipping)}</span>
         </p>
         {/* TAX */}
-        <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+        <p className='flex justify-between font-medium text-sm border-b border-base-300 pb-2'>
           <span>Tax</span>
-          <span className='font-medium'>{formatPrice(tax)}</span>
+          <span className='font-semibold'>{formatPrice(tax)}</span>
         </p>
         {/* ORDER TOTAL */}
-        <p className='flex justify-between text-sm mt-4 pb-2'>
+        <p className='flex justify-between text-md mt-4 pb-2'>
           <span className='font-bold'>Order Total</span>
           <span className='font-bold'>{formatPrice(orderTotal)}</span>
         </p>
